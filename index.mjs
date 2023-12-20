b66493e91c418b00e52007775448262c0408c4a7
majid chakeri <majidchakeri@outlook.com>
2023-12-20 19:05:19 +0330

    build add e2e test for Users rout to project and add readme file

--- index.mjs
+++ index.mjs
@@ -5,14 +5,17 @@ function delay(time) {
 		setTimeout(resolve, time);
 	});
 }
+
 (async () => {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
 
 	await page.goto("http://localhost:3000/");
 
-	await page.setViewport({ width: 1080, height: 1024 });
+	await page.setViewport({ width: 1366, height: 768 });
 	await delay(2000);
+
+	console.log("Test for verification rout is started...");
 
 	await page.click("div a");
 	await delay(2000);
@@ -32,7 +35,6 @@ function delay(time) {
 	await delay(2000);
 
 	const selectors = await page.$x("/html/body/div[1]/button[2]");
-	console.log("selector is ==== ", selectors);
 	await delay(2000);
 	await selectors[0].click();
 
@@ -43,5 +45,48 @@ function delay(time) {
 	await delay(4000);
 
 	console.log("Test for verification rout is END...");
-	await browser.close();
+
+	UsersTest(page, browser);
 })();
+
+async function UsersTest(page, browser) {
+	console.log("Test for users rout is started...");
+
+	const usersRout = await page.$x("/html/body/div[1]/div/a[2]");
+	await delay(2000);
+	await usersRout[0].click();
+
+	await delay(2000);
+
+	await page.keyboard.down("Control");
+	await page.keyboard.down("Shift");
+	await page.keyboard.press("F2");
+	await page.keyboard.up("Shift");
+	await page.keyboard.up("Control");
+
+	await page.waitForTimeout(2000);
+
+	await page.keyboard.down("Control");
+	await page.keyboard.down("Shift");
+	await page.keyboard.press("F3");
+	await page.keyboard.up("Shift");
+	await page.keyboard.up("Control");
+
+	await delay(2000);
+	await page.keyboard.type("It's test for type and showing dialog box value");
+	await delay(4000);
+
+	await page.keyboard.press("Escape");
+
+	await page.waitForTimeout(2000);
+
+	console.log("Test for users rout is END...");
+
+	await page.waitForTimeout(2000);
+
+	await page.goBack();
+
+	await page.waitForTimeout(2000);
+
+	await browser.close();
+}
